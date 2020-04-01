a98eca2d0ca369b286437c11bc7b91e6f8a44c43
Ulukele <arkhip2002@gmail.com>
2020-04-02 04:03:41 +0700

    add content for response

--- yandex_lambda/handler.py
+++ yandex_lambda/handler.py
@@ -3,24 +3,44 @@
 import subprocess
 from verdict_codes import *
 
-PYTHON_VERSION = 'python3.6'
+PYTHON_VERSION = 'python3.7'
 
 def handler(event, context):
 
     if 'queryStringParameters' not in event:
         return {
-            'statusCode': CHECKER_ERROR
+            'statusCode': CHECKER_ERROR,
+            'headers': {
+                'Content-Type': 'text/plain'
+            },
+            'isBase64Encoded': False,
+            'body': ""
         }
 
     for parameter in ('ip', 'port', 'round'):
         if parameter not in event['queryStringParameters']:
             return {
-                'statusCode': CHECKER_ERROR
+                'statusCode': CHECKER_ERROR,
+                'headers': {
+                    'Content-Type': 'text/plain'
+                },
+                'isBase64Encoded': False,
+                'body': ""
             }
     
     response = subprocess.run([PYTHON_VERSION, 'main.py', '--ip', event['queryStringParameters']['ip'], '--port', event['queryStringParameters']['port'], '--round', event['queryStringParameters']['round']], stdout=subprocess.PIPE)
-    response_text = response.stdout.decode('utf-8')
+    response_text = response.stdout.decode('utf-8').split('\n')[:-1]
+
+    response_error, response_code = None, None
+    if len(response_text) > 1:
+        response_error = response_text[0]
+    response_code = response_text[-1]
 
     return {
-        'statusCode': response_text
+        'statusCode': response_code,
+        'headers': {
+            'Content-Type': 'text/plain'
+        },
+        'isBase64Encoded': False,
+        'body': ""
     }
