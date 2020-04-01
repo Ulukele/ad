534b069411a44a7afa33797626d15b6de46a5c88
Ulukele <arkhip2002@gmail.com>
2020-04-02 02:56:23 +0700

    add verdict codes constants, add them into the handler.py

--- yandex_lambda/handler.py
+++ yandex_lambda/handler.py
@@ -1,6 +1,7 @@
 """ main handler that handle http requests to yandex cloud """
 
 import subprocess
+from verdict_codes import *
 
 NO_PARAMETERS = 404 #idk what code should be returned to such a call
 PYTHON_VERSION = 'python3.6'
@@ -9,18 +10,18 @@ def handler(event, context):
 
     if 'queryStringParameters' not in event:
         return {
-            'statusCode': NO_PARAMETERS
+            'statusCode': CHECKER_ERROR
         }
 
     for parameter in ('ip', 'port', 'round'):
         if parameter not in event['queryStringParameters']:
             return {
-                'statusCode': NO_PARAMETERS
+                'statusCode': CHECKER_ERROR
             }
     
     response = subprocess.run([PYTHON_VERSION, 'main.py', '--ip', event['queryStringParameters']['ip'], '--port', event['queryStringParameters']['port'], '--round', event['queryStringParameters']['round']], stdout=subprocess.PIPE)
     response_text = response.stdout.decode('utf-8')
 
     return {
-        'statusCode': 102
+        'statusCode': response_text
     }
--- yandex_lambda/main.py
+++ yandex_lambda/main.py
@@ -1,5 +1,7 @@
 """ checker entrypoint """
 
+from verdict_codes import *
+
 if __name__ == '__main__':
-    print("hi, i am checker")
+    print(OK)
     
