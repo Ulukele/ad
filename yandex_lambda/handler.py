486b4f3e26f7404889b3da8221b5493475ce96c1
Ulukele <arkhip2002@gmail.com>
2020-04-03 21:45:05 +0700

    remove sending status to queue from lambda

--- yandex_lambda/handler.py
+++ yandex_lambda/handler.py
@@ -1,56 +1,9 @@
 """ main handler that handle http requests to yandex cloud """
 
-import boto3
 import subprocess
 from verdict_codes import *
 
 PYTHON_VERSION = 'python3.7'
-
-def status_to_queue(ip, port, round, status):
-
-    # Get keys to service account
-    aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-    aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
-    
-    # Create client
-    client = boto3.client(
-        service_name='sqs',
-        endpoint_url='https://message-queue.api.cloud.yandex.net',
-        region_name='ru-central1',
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key
-    )
-
-
-    # Create queue and get its url
-    queue_url = client.create_queue(QueueName='checker-response-queue').get('QueueUrl')
-    print('Created queue url is "{}"'.format(queue_url))
-
-
-    # Send message to queue
-    client.send_message(
-        QueueUrl=queue_url,
-        MessageBody='{},{},{},{}'.format(ip, port, round, status),
-        MessageAttributes={
-                    "ip": {
-                        "StringValue": str(ip),
-                        "DataType": "Number"
-                    },
-                    "port": {
-                        "StringValue": str(port),
-                        "DataType": "Number"
-                    },
-                    "round": {
-                        "StringValue": str(round),
-                        "DataType": "Number"
-                    },
-                    "status": {
-                        "StringValue": str(status),
-                        "DataType": "Number"
-                    }
-                }
-    )
 
 def handle_http(event):
 
@@ -102,7 +55,6 @@ def handle_queue_message(event):
             response_error = response_text[0]
         response_code = response_text[-1]
 
-        status_to_queue(ip, port, round, response_code)
         return {
             'statusCode': response_code,
             'headers': {
@@ -112,7 +64,6 @@ def handle_queue_message(event):
             'body': response_error
         }
     except:
-        status_to_queue(ip, port, round, CHECKER_ERROR)
         return YC_ERROR
 
 def handler(event, context):
