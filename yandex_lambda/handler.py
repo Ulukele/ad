dc771f8826e22509ee326da4170a205e5971d343
Ulukele <arkhip2002@gmail.com>
2020-04-03 21:05:45 +0700

    add sending status into queue

--- yandex_lambda/handler.py
+++ yandex_lambda/handler.py
@@ -1,15 +1,62 @@
 """ main handler that handle http requests to yandex cloud """
 
+import boto3
 import subprocess
 from verdict_codes import *
 
 PYTHON_VERSION = 'python3.7'
 
+def status_to_queue(ip, port, round, status):
+
+    #TODO add aws_access_key_id and aws_secret_access_key
+
+    # Create client
+    client = boto3.client(
+        service_name='sqs',
+        endpoint_url='https://message-queue.api.cloud.yandex.net',
+        region_name='ru-central1',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
+
+
+    # Create queue and get its url
+    queue_url = client.create_queue(QueueName='checker-response-queue').get('QueueUrl')
+    print('Created queue url is "{}"'.format(queue_url))
+
+
+    # Send message to queue
+        client.send_message(
+            QueueUrl=queue_url,
+            MessageBody='{},{},{},{}'.format(ip, port, round, status),
+            MessageAttributes={
+                        "ip": {
+                            "StringValue": str(ip),
+                            "DataType": "Number"
+                        },
+                        "port": {
+                            "StringValue": str(port),
+                            "DataType": "Number"
+                        },
+                        "round": {
+                            "StringValue": str(round),
+                            "DataType": "Number"
+                        }
+                        "status": {
+                            "StringValue": str(status),
+                            "DataType": "Number"
+                        }
+                    }
+        )
+
 def handle_http(event):
+
+    # Check if ip, port, round in queryStringParameters
     for parameter in ('ip', 'port', 'round'):
         if parameter not in event['queryStringParameters']:
             return YC_ERROR
     
+    # Run checker
     response = subprocess.run([PYTHON_VERSION, 'main.py', '--ip', event['queryStringParameters']['ip'], '--port', event['queryStringParameters']['port'], '--round', event['queryStringParameters']['round']], stdout=subprocess.PIPE)
     response_text = response.stdout.decode('utf-8').split('\n')[:-1]
 
@@ -18,6 +65,7 @@ def handle_http(event):
         response_error = response_text[0]
     response_code = response_text[-1]
 
+    
     return {
         'statusCode': response_code,
         'headers': {
@@ -29,7 +77,11 @@ def handle_http(event):
 
 def handle_queue_message(event):
 
+
     try:
+        # Trigger can get more than 1 message
+        # To recieve all messages trigger should take only 1 message in group
+        # Get first message
         message = event['messages'][0]
 
         parameters = message['details']['message']['message_attributes']
@@ -37,6 +89,8 @@ def handle_queue_message(event):
         port = parameters['port']['stringValue']
         round = parameters['round']['stringValue']
 
+
+        # Run checker
         response = subprocess.run([PYTHON_VERSION, 'main.py', '--ip', ip, '--port', port, '--round', round], stdout=subprocess.PIPE)
         response_text = response.stdout.decode('utf-8').split('\n')[:-1]
 
@@ -45,6 +99,7 @@ def handle_queue_message(event):
             response_error = response_text[0]
         response_code = response_text[-1]
 
+        status_to_queue(ip, port, round, response_code)
         return {
             'statusCode': response_code,
             'headers': {
@@ -54,6 +109,7 @@ def handle_queue_message(event):
             'body': response_error
         }
     except:
+        status_to_queue(ip, port, round, CHECKER_ERROR)
         return YC_ERROR
 
 def handler(event, context):
